c1b7c6f366d59f9a4ba00042df79368cfa05ba87
Denis <caribu1996@gmail.com>
2021-03-10 13:57:13 +0300

    New function

--- AdVKScript.js
+++ AdVKScript.js
@@ -24,3 +24,28 @@ function showAdOnBegin(){
         });
      }
 }
+
+function showAdOnGame(){
+   if (window.location.toString().indexOf('vk.com') > 0 || window.location.toString().indexOf('m.vk.com') > 0) {
+      console.log('Start load...');
+      var user_id = null; // user's id
+      var app_id = 7776768; // your app's id
+
+      admanInit({
+         user_id: user_id,
+         app_id: 7776768,
+         //params: {preview: 1}, // to verify the correct operation of advertising
+         type: 'preloader' // 'preloader' or 'rewarded' (default - 'preloader')
+      }, onAdsReady, onNoAds);
+
+      function onAdsReady(adman) {
+         adman.onStarted(function () { console.log('Ad started...'); });
+         adman.onCompleted(function () { console.log('Ad completed...'); });
+         adman.onSkipped(function () { console.log('Ad skipped...'); });
+         adman.onClicked(function () { console.log('Ad clicked...'); });
+         adman.start('preroll');
+      };
+
+      function onNoAds() { console.log('No ad...'); };
+   }
+}
