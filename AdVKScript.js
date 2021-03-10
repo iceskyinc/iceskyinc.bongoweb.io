6f38cf1b8df8d4a63bcb0847daf64d0e48c7db85
Denis <caribu1996@gmail.com>
2021-03-10 13:49:23 +0300

    Create vk script

---
+++ AdVKScript.js
@@ -0,0 +1,26 @@
+function showAdOnBegin(){
+    if (window.location.toString().indexOf('vk.com') > 0 || window.location.toString().indexOf('m.vk.com') > 0) {
+       console.log('Load ad on begin...');
+        window.addEventListener('load', function () {
+
+           var user_id = null;   // user's id
+           var app_id = 7776768;  // your app's id
+
+           admanInit({
+              user_id: user_id,
+              app_id: 7776768,
+              type: 'preloader'         // 'preloader' or 'rewarded' (default - 'preloader')
+              // params: {preview: 1}   // to verify the correct operation of advertising
+           }, onAdsReady, onNoAds);
+
+           function onAdsReady(adman) {
+              adman.onStarted(function () { console.log('Ad started...'); });
+              adman.onCompleted(function () { console.log('Ad completed...'); });
+              adman.onSkipped(function () { console.log('Ad skipped...'); });
+              adman.onClicked(function () { console.log('Ad clicked...'); });
+              adman.start('preroll');
+           };
+           function onNoAds() { console.log('No ad...'); };
+        });
+     }
+}
